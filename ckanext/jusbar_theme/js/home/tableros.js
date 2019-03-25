638276bf59e3d81272e4c4882918ed06ac6d054c
Xavier Petit <nuxion@gmail.com>
2019-03-24 21:23:34 -0300

    tableros y pages

--- ckanext/jusbar_theme/js/home/tableros.js
+++ ckanext/jusbar_theme/js/home/tableros.js
@@ -1,8 +1,8 @@
 $(function () {
     var animationDelay = 50;
     //var calls = $('.featured-groups img[data-replace="svg"]').replaceSVG();
-    var calls = $('.minjus img[data-replace="svg"]').replaceSVG();
-    console.log($('.minjus img[data-replace="svg"]'));
+    var calls = $('#tablero .panel img').replaceSVG();
+    //console.log(calls);
     var callback = function () {
         $.each($('.lalalagroup-container'), function (i, el) {
         //$.each($('.minjus-group'), function (i, el) {
